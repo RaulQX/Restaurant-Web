d9f882792a19a4411ca9dacc7934f2528a28e0f8
RaulQX <raul.sarbu@student.upt.ro>
2022-03-25 09:53:15 +0200

    Added most pages

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+	<div className="app__bg app__wrapper section__padding">
+		<div className="app__wrapper_img app__wrapper_img-reverse">
+			<img src={images.chef} alt="chef" />
+		</div>
+
+		<div className="app__wrapper_info">
+			<SubHeading title={"Chef's Words"} />
+			<h1 className="headtext__cormorant">What we believe in</h1>
+			<div className="app__chef-content">
+				<div className="app__chef-content_quote">
+					<img src={images.quote} alt="quote"></img>
+					<p className="p__opensans">
+						Chef says what the chef wants to say because the chef says that he says what
+						he wants to say yes lorem ipsum
+					</p>
+				</div>
+				<p className="p__opensans">
+					e chef says that he says what he wants to say yes lorem ipsume chef says that he
+					says what he wants to say yes lorem ipsume chef says that he says what he wants
+					to say yes lorem ipsume chef says that he says what he wants to say yes lorem
+					ipsum
+				</p>
+			</div>
+			<div className="app__chef-sign">
+				<p>Kevin Luo</p>
+				<p className="p__opensans">Chef & Founder</p>
+				<img src={images.sign} alt="sign" />
+			</div>
+		</div>
+	</div>
 );
 
 export default Chef;
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { BsPlayFill } from "react-icons/bs";
+import "./Intro.css";
+import { meal } from "../../constants";
+import { useState } from "react";
 
-import './Intro.css';
+const Intro = () => {
+	const [playVideo, setPlayVideo] = useState(false);
+	const vidRef = React.useRef();
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+	const handleVideo = () => {
+		setPlayVideo(!playVideo);
 
+		if (playVideo) {
+			vidRef.current.pause();
+		} else {
+			vidRef.current.play();
+		}
+	};
+
+	return (
+		<div className="app__video">
+			<video ref={vidRef} src={meal} type="video/mp4" loop controls={false} muted />
+			<div className="app__video-overlay flex__center" onClick={handleVideo}>
+				{!playVideo && (
+					<div className="app__video-overlay_circle flex__center">
+						<BsPlayFill color="#fff" fontSize={30} />
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
 export default Intro;
